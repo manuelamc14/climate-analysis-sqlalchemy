3b5e2c743b263a2ffe74117b4d052546399fdb28
Manuela Munoz <manuelamunoz103@gmail.com>
2020-11-07 20:49:55 -0500

    Flask

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Dependencies
 
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -13,3 +12,80 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+print(Base.classes.keys())
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome(): 
+    """List all availabe api routes"""
+    return (
+        f"Welcome!<br?>"
+        f" What are you looking for? These are all availabe API routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/2017-08-23<br/>"
+        f"/api/v1.0/2010-01-01/2017-08-23"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+    # Create the session (link) from Python to the DB
+    session = Session(engine)
+
+    precipitation_inf = session.query(Measurement.date, Measurement.prcp)
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of precipitation_list
+    precipitation_list = []
+    for date, prcp in precipitation_inf:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"]= prcp  
+        precipitation_list.append(precipitation_dict)
+
+    return jsonify(precipitation_list)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create the session (link) from Python to the DB
+    session = Session(engine)
+
+    stations_query = session.query(Station.station).distinct().all()
+
+    session.close
+
+    # Convert list of tuples into normal list
+
+    stations_list = list(np.ravel(stations_query))
+
+    return jsonify(stations_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
